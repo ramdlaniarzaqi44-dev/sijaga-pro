791fd50adc38f12d098addadaf490a2e43db6159
ramdlaniarzaqi44-dev <ramdlaniarzaqi44@guru.sma.belajar.id>
2026-05-11 12:20:21 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
 VitePWA({ // 
       registerType: 'autoUpdate',
+  injectRegister: 'auto',
   devOptions: {
         enabled: true // <-- Tambahkan baris ini agar jalan di localhost
       },
