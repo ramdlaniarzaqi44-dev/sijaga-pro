f366603fc2495b2a8eb9b62cf9313cd6f9e4a655
ramdlaniarzaqi44-dev <ramdlaniarzaqi44@guru.sma.belajar.id>
2026-05-11 11:57:08 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+VitePWA({ // 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Sijaga Pro',
+        short_name: 'Sijaga',
+        description: 'Aplikasi Sijaga',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'SIJAGAPRO 192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'SIJAGAPRO 512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
